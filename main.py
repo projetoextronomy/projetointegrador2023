62a25536b907529413b79470a60b62acf0530704
unknown <anaclara.semensato@hotmail.com>
2023-11-22 07:15:09 -0300

    terceira versao

--- main.py
+++ main.py
@@ -35,5 +35,5 @@ app.include_router(usuarioRoutes)
 app.include_router(postagemRoutes)
 
 
-if __name__ =="__main__":
-    uvicorn.run(app="main:app", reload=True, port=8000)
+#if __name__ =="__main__":
+    #uvicorn.run(app="main:app", reload=True, port=8000)
